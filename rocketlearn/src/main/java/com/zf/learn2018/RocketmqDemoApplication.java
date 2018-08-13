5b9aed71843a29e05d2c51e33071efd6d2c558da
zfgod <645561678@qq.com>
2018-08-13 16:00:42 +0800

    rocketmq model proj

---
+++ rocketlearn/src/main/java/com/zf/learn2018/RocketmqDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zf.learn2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketmqDemoApplication.class, args);
+	}
+}
